5536dcf00abc0777b3c271609d90cc06466c3b4e
David Konsumer <konsumer@jetboystudio.com>
2023-02-18 01:26:36 -0800

    initial libretro core

--- libretro/null0_libretro.nim
+++ libretro/null0_libretro.nim
@@ -1,51 +1,11 @@
-import print
+import bitops
+import std/terminal
+import libretro
 
-type
-  RetroGameGeometry {.bycopy.} = object
-    baseWidth: cuint
-    baseHeight: cuint
-    maxWidth: cuint
-    maxHeight: cuint
-    aspectRatio: cfloat
-
-  RetroSystemTiming {.bycopy.} = object
-    fps: cdouble
-    sampleRate: cdouble
-
-  RetroSystemAvInfo {.bycopy.} = object
-    geometry: RetroGameGeometry
-    timing: RetroSystemTiming
-
-  RetroSystemInfo* {.bycopy.} = object
-    libraryName*: cstring
-    libraryVersion*: cstring
-    validExtensions*: cstring
-    needFullpath: bool
-    blockExtract: bool
-  
-  RetroGameInfo {.bycopy.} = object
-    path: cstring
-    data: pointer
-    size: csize_t
-    meta: cstring
-
-  # typedef void(RETRO_CALLCONV* retro_video_refresh_t)(const void* data, unsigned width, unsigned height, size_t pitch);
-  retro_video_refresh_t = (proc(data:array[307200, byte], width:cuint, height:cuint, pitch:csize_t):void) # 320 * 240 * 4
-
-  # typedef void(RETRO_CALLCONV* retro_audio_sample_t)(int16_t left, int16_t right);
-  retro_audio_sample_t = (proc(left:int16, right:int16):void)
-
-  # typedef size_t(RETRO_CALLCONV* retro_audio_sample_batch_t)(const int16_t* data, size_t frames);
-  retro_audio_sample_batch_t = (proc(data:array[800, int16], frames:csize_t):csize_t) # 48000/60
-
-  # typedef void(RETRO_CALLCONV* retro_input_poll_t)(void);
-  retro_input_poll_t = (proc():void)
-
-  # typedef bool(RETRO_CALLCONV* retro_environment_t)(unsigned cmd, void* data);
-  retro_environment_t = (proc(cmd:cuint, data:pointer):bool)
-  
-  # typedef int16_t(RETRO_CALLCONV* retro_input_state_t)(unsigned port, unsigned device, unsigned index, unsigned id);
-  retro_input_state_t = (proc(port:cuint, device:cuint, index:cuint, id:cuint):int16)
+const WIDTH = 320
+const HEIGHT = 240 
+const FPS = 60
+const SAMPLE_RATE = 48000
 
 var video_cb: retro_video_refresh_t
 var audio_cb: retro_audio_sample_t
@@ -54,7 +14,22 @@ var input_poll_cb: retro_input_poll_t
 var environ_cb: retro_environment_t
 var input_state_cb: retro_input_state_t
 
-# TODO: these currently segfault
+# video framebuffer
+var buf = newSeq[cuint](WIDTH * HEIGHT)
+
+# colors used for checkerboard
+const color_r:uint32 = 0xff shl 16
+const color_g:uint32 = 0xff shl 8
+
+proc log_cb(level: retro_log_level, message: string) =
+  if level == RETRO_LOG_DEBUG:
+    stdout.styledWriteLine(fgBlue, "DEBUG: ", fgDefault, message)
+  elif level == RETRO_LOG_INFO:
+    stdout.styledWriteLine(fgYellow, "INFO: ", fgDefault, message)
+  elif level == RETRO_LOG_WARN:
+    stdout.styledWriteLine(bgMagenta, "WARN: ", fgDefault, message)
+  elif level == RETRO_LOG_ERROR:
+    stdout.styledWriteLine(fgRed, "ERROR: ", fgDefault, message)
 
 proc retro_set_video_refresh*(cb: retro_video_refresh_t) {.cdecl,exportc,dynlib.} =
   video_cb = cb
@@ -70,59 +45,69 @@ proc retro_set_input_poll*(cb: retro_input_poll_t) {.cdecl,exportc,dynlib.} =
 
 proc retro_set_environment*(cb: retro_environment_t) {.cdecl,exportc,dynlib.} =
   environ_cb = cb
+  var no_content:bool = true
+  discard cb(RETRO_ENVIRONMENT_SET_SUPPORT_NO_GAME, addr no_content)
 
 proc retro_set_input_state*(cb: retro_input_state_t) {.cdecl,exportc,dynlib.} =
   input_state_cb = cb
 
-
-
 proc retro_init*() {.cdecl,exportc,dynlib.} =
-  echo "retro_init"
+  log_cb(RETRO_LOG_DEBUG, "retro_init() called.")
 
 proc retro_deinit*() {.cdecl,exportc,dynlib.} =
-  echo "retro_deinit"
+  log_cb(RETRO_LOG_DEBUG, "retro_deinit() called.")
 
 proc retro_api_version*(): cuint {.cdecl,exportc,dynlib.} =
-  return 1
+  return RETRO_API_VERSION
 
 proc retro_set_controller_port_device*(port: cuint; device: cuint) {.cdecl,exportc,dynlib.} =
-  echo "retro_set_controller_port_device"
+  log_cb(RETRO_LOG_DEBUG, "retro_set_controller_port_device() called.")
   echo port, device
 
-proc retro_get_system_info*(info: ptr RetroSystemInfo) {.cdecl,exportc,dynlib.} =
-  info.library_name = "null0";
+proc retro_get_system_info*(info: ptr retro_system_info) {.cdecl,exportc,dynlib.} =
+  info.library_name = "nim_example";
   info.library_version = "v1"
-  info.need_fullpath = true
-  info.valid_extensions = "null0|wasm|zip"
+  info.need_fullpath = false
+  info.valid_extensions = nil # we don't use any ROMs
 
-proc retro_get_system_av_info*(info: ptr RetroSystemAvInfo) {.cdecl,exportc,dynlib.} =
-  echo "retro_get_system_av_info"
-  info.timing.fps = 60
-  info.timing.sample_rate = 48000
-  info.geometry.base_width = 320
-  info.geometry.base_height = 240
-  info.geometry.max_width = 320
-  info.geometry.max_height = 240
-  info.geometry.aspect_ratio = 4 / 3
+proc retro_get_system_av_info*(info: ptr retro_system_av_info) {.cdecl,exportc,dynlib.} =
+  info.timing.fps = FPS
+  info.timing.sample_rate = SAMPLE_RATE
+  info.geometry.base_width = WIDTH
+  info.geometry.base_height = HEIGHT
+  info.geometry.max_width = WIDTH
+  info.geometry.max_height = HEIGHT
+  info.geometry.aspect_ratio = WIDTH / HEIGHT
 
 proc retro_reset*() {.cdecl,exportc,dynlib.} =
-  echo "retro_reset"
+  log_cb(RETRO_LOG_DEBUG, "retro_reset() called.")
 
 proc retro_run*() {.cdecl,exportc,dynlib.} =
-  echo "retro_run"
+  for y in 0..(HEIGHT - 1):
+    let index_y = uint32 bitand((y shr 4), 1)
+    for x in 0..(WIDTH - 1):
+      let b = ((y * WIDTH) + x)
+      let index_x = uint32 bitand((x shr 4), 1)
+      if bool bitxor(index_y, index_x):
+        buf[b] = color_r
+      else:
+        buf[b] = color_g
+  video_cb(buf, WIDTH, HEIGHT, (WIDTH shl 2))
 
-proc retro_load_game*(info: ptr RetroGameInfo): bool {.cdecl,exportc,dynlib.} =
-  echo "retro_load_game"
-  print(info)
+proc retro_load_game*(info: ptr retro_game_info): bool {.cdecl,exportc,dynlib.} =
+  var fmt = RETRO_PIXEL_FORMAT_XRGB8888
+  if not environ_cb(RETRO_ENVIRONMENT_SET_PIXEL_FORMAT, addr fmt):
+    log_cb(RETRO_LOG_INFO, "XRGB8888 is not supported.")
+    return false
   return true
 
 proc retro_unload_game*() {.cdecl,exportc,dynlib.} =
-  echo "retro_unload_game"
+  log_cb(RETRO_LOG_DEBUG, "retro_unload_game() called.")
 
 proc retro_get_region*(): cuint {.cdecl,exportc,dynlib.} =
   return 0 # NTSC
 
-proc retro_load_game_special*(`type`: cuint; info: ptr RetroGameInfo; num: csize_t): bool {.cdecl,exportc,dynlib.} =
+proc retro_load_game_special*(`type`: cuint; info: ptr retro_game_info; num: csize_t): bool {.cdecl,exportc,dynlib.} =
   return true
 
 proc retro_serialize_size*(): csize_t {.cdecl,exportc,dynlib.} =
