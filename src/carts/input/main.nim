0696902300ac96f88882c37b59f47e1c7d9c326f
David Konsumer <konsumer@jetboystudio.com>
2023-02-25 21:26:42 -0800

    nicer input demo

--- src/carts/input/main.nim
+++ src/carts/input/main.nim
@@ -1,47 +1,41 @@
-# not suire if this is needed, but was getting "(invalid data!)" trying to echo
-proc getName(button: Button): string = 
-  case button:
-    of BUTTON_B:
-      return "B"
-    of BUTTON_Y:
-      return "Y"
-    of BUTTON_SELECT:
-      return "SELECT"
-    of BUTTON_START:
-      return "START"
-    of BUTTON_UP:
-      return "UP"
-    of BUTTON_DOWN:
-      return "DOWN"
-    of BUTTON_LEFT:
-      return "LEFT"
-    of BUTTON_RIGHT:
-      return "RIGHT"
-    of BUTTON_A:
-      return "A"
-    of BUTTON_X:
-      return "X"
-    of BUTTON_L:
-      return "L"
-    of BUTTON_R:
-      return "R"
-    of BUTTON_L2:
-      return "L2"
-    of BUTTON_R2:
-      return "R2"
-    of BUTTON_L3:
-      return "L3"
-    of BUTTON_R3:
-      return "R3"
+var buttonState:array[0..15, bool]
+var buttonImages:array[0..15, uint8]
+var bg:uint8
 
 proc load() {.null0.} =
   echo "Hello from input."
+  bg = load_image("assets/bg.png")
+  buttonImages[ord BUTTON_B] = load_image("assets/b.png")
+  buttonImages[ord BUTTON_Y] = load_image("assets/y.png")
+  buttonImages[ord BUTTON_SELECT] = load_image("assets/select.png")
+  buttonImages[ord BUTTON_START] = load_image("assets/start.png")
+  buttonImages[ord BUTTON_UP] = load_image("assets/up.png")
+  buttonImages[ord BUTTON_DOWN] = load_image("assets/down.png")
+  buttonImages[ord BUTTON_LEFT] = load_image("assets/left.png")
+  buttonImages[ord BUTTON_RIGHT] = load_image("assets/right.png")
+  buttonImages[ord BUTTON_A] = load_image("assets/a.png")
+  buttonImages[ord BUTTON_X] = load_image("assets/x.png")
+  buttonImages[ord BUTTON_L] = load_image("assets/l.png")
+  buttonImages[ord BUTTON_R] = load_image("assets/r.png")
+
+  # I wasn't sure how to draw these in 2D, so I just merged them into L/R
+  buttonImages[ord BUTTON_L2] = load_image("assets/l.png")
+  buttonImages[ord BUTTON_R2] = load_image("assets/r.png")
+  buttonImages[ord BUTTON_L3] = load_image("assets/l.png")
+  buttonImages[ord BUTTON_R3] = load_image("assets/r.png")
 
 proc buttonDown(button: Button) {.null0.} =
-  echo "down: ", getName(button)
+  buttonState[ord button] = true
 
 proc buttonUp(button: Button) {.null0.} =
-  echo "up: ", getName(button)
+  buttonState[ord button] = false
+
+proc update() {.null0.} =
+  clear_background(RED)
+  draw_image(bg, 0, 50)
+  for b,state in pairs(buttonState):
+    if state:
+      draw_image(buttonImages[b], 0, 3)
 
 proc unload() {.null0.} =
   echo "Ok, bye."
