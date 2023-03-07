a9d6feba49d2e47dab612a2d671063e0ea5571d7
David Konsumer <konsumer@jetboystudio.com>
2023-03-07 01:38:44 -0800

    basic working sound

--- tests/test_soloud.nim
+++ tests/test_soloud.nim
@@ -1,29 +1,16 @@
-import times
 import os
 import unittest
 
 import ../src/null0/soloud
-import ../src/null0/physfs
 
 suite "Soloud":
-  test "play ogg":
+  test "text":
     var sl = Soloud_create()
-    discard Soloud_initEx(sl, ord(SOLOUD_CLIP_ROUNDOFF), ord(SOLOUD_NULLDRIVER), 44100, ord(SOLOUD_AUTO), 2)
-    Soloud_setGlobalVolume(sl, 1)
-
-    check physfs.init("test")
-    check physfs.mount("sound.null0", "", true)
-    var b = physfs.read("assets/notnullgames.ogg")
-    var stream = WavStream_create()
-    var success = WavStream_loadMem(stream, cast[ptr uint8](b.data), cuint b.length)
-
-    let currentTime = epochTime()
-    let length = WavStream_getLength(stream)
-    discard Soloud_play(cast[ptr Soloud](sl), cast[ptr Wav](stream))
-
-    while epochTime() - currentTime <= length:
+    discard Soloud_initEx(sl, CLIP_ROUNDOFF, MINIAUDIO, 44100, 0, 2)
+    # discard Soloud_init(sl)
+    Soloud_setGlobalVolume(sl, 3.0)
+    var speech = Speech_create()
+    discard Speech_setText(speech, "Hello")
+    discard Soloud_play(sl, speech)
+    while Soloud_getVoiceCount(sl) > 0:
       sleep(100)
-
-    Soloud_deinit(sl)
-    Soloud_destroy(sl)
-    discard physfs.deinit()
