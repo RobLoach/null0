8b116ebd9ef92dee8d32d933e0984a15e5c7f4eb
David Konsumer <konsumer@jetboystudio.com>
2023-03-07 22:59:44 -0800

    use defer more

--- tests/test_physfs.nim
+++ tests/test_physfs.nim
@@ -4,8 +4,9 @@ import ../src/null0/physfs
 suite "Physfs":
   test "mount zip file and read from it using read()":
     check physfs.init("test")
+    defer: discard physfs.deinit()
+    
     check physfs.mount("draw.null0", "", true)
     var b = physfs.read("assets/logo.png")
     var l = physfs.fileLength("assets/logo.png")
     check l == 9211
-    discard physfs.deinit()
--- tests/test_pntr.nim
+++ tests/test_pntr.nim
@@ -5,52 +5,64 @@ import ../src/null0/physfs
 suite "Pntr: Gradients":
   test "horizontal":
     let canvas = pntr.gen_image_gradient_horizontal(320, 240, RED, BLUE)
+    defer: pntr.unload_image(canvas)
+
     pntr.save_image(canvas, "test-gradient-horiz.png")
-    pntr.unload_image(canvas)
 
   test "vertical":
     let canvas = pntr.gen_image_gradient_vertical(320, 240, RED, BLUE)
+    defer: pntr.unload_image(canvas)
+
     pntr.save_image(canvas, "test-gradient-vert.png")
-    pntr.unload_image(canvas)
 
 suite "Pntr: BM Fonts":
   test "from file":
     let font = pntr.load_bmfont("./vendor/pntr/examples/resources/bmfont.png", " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,!?-+/")
+    defer: pntr.unload_font(font)
+
     let canvas = pntr.gen_image_color(320, 240, BLACK)
+    defer: pntr.unload_image(canvas)
+
     pntr.draw_text(canvas, font, "Hello World", 120, 100)
     pntr.save_image(canvas, "test-bm-file.png")
-    pntr.unload_font(font)
-    pntr.unload_image(canvas)
 
-  test "from memory":
+  test "from physfs":
     check physfs.init("test")
+    defer: discard physfs.deinit()
+
     check physfs.mount("fonts.null0", "", true)
     var b = physfs.read("/assets/bmfont.png")
     let font = pntr.load_bmfont_from_memory(b.data, cuint b.length, " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,!?-+/")
+    defer: pntr.unload_font(font)
+
     let canvas = pntr.gen_image_color(320, 240, BLACK)
+    defer: pntr.unload_image(canvas)
+
     pntr.draw_text(canvas, font, "Hello World", 120, 100)
     pntr.save_image(canvas, "test-bm-mem.png")
-    discard physfs.deinit()
-    pntr.unload_font(font)
-    pntr.unload_image(canvas)
 
 suite "Pntr: TTF Fonts":
   test "from file":
     let font = pntr.load_ttffont("./vendor/pntr/examples/resources/tuffy.ttf", 20, WHITE)
+    defer: pntr.unload_font(font)
+
     let canvas = pntr.gen_image_color(320, 240, BLACK)
+    defer: pntr.unload_image(canvas)
+
     pntr.draw_text(canvas, font, "Hello World", 120, 100)
     pntr.save_image(canvas, "test-ttf-file.png")
-    pntr.unload_font(font)
-    pntr.unload_image(canvas)
 
-  test "from memory":
+  test "from physfs":
     check init("test")
+    defer: discard physfs.deinit()
+
     check mount("fonts.null0", "", true)
     var b = read("/assets/tuffy.ttf")
     let font = load_ttffont_from_memory(b.data, cuint b.length, 20, WHITE)
+    defer: pntr.unload_font(font)
+
     let canvas = gen_image_color(320, 240, BLACK)
+    defer: pntr.unload_image(canvas)
+
     draw_text(canvas, font, "Hello World", 120, 100)
     save_image(canvas, "test-ttf-mem.png")
-    discard deinit()
-    pntr.unload_font(font)
-    pntr.unload_image(canvas)
