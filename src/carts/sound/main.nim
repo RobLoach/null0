52fe19218c9f1ad3b57f11bd5e9c99022929fdf7
David Konsumer <konsumer@jetboystudio.com>
2023-03-17 18:48:47 -0700

    add buttons for both sound types

--- src/carts/sound/main.nim
+++ src/carts/sound/main.nim
@@ -5,14 +5,15 @@ proc load*() {.null0.} =
   echo "Hello from sound."
   intro = load_speech("Hello World.")
   nnn = load_sound("/assets/notnullgames.ogg")
-  play_sound(nnn)
-
+  
 proc update() {.null0.} =
-  draw_text("Press A", 140, 110)
+  draw_text("Press A / B", 100, 110)
 
 proc buttonDown(button: Button) {.null0.} =
   if button == BUTTON_A:
     play_sound(intro)
+  if button == BUTTON_B:
+    play_sound(nnn)
 
 proc unload*() {.null0.} =
   echo "Ok, bye."
