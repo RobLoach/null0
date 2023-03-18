8b35b6a147930c821d844fd2e06871edefa7abff
David Konsumer <konsumer@jetboystudio.com>
2023-03-17 19:38:35 -0700

    background music for sound demo

--- src/carts/sound/main.nim
+++ src/carts/sound/main.nim
@@ -1,10 +1,13 @@
 var intro:uint8
-var nnn:uint8
+var notnullgames:uint8
+var forgotten_spaceship:uint8
 
 proc load*() {.null0.} =
   echo "Hello from sound."
   intro = load_speech("Hello World.")
-  nnn = load_sound("/assets/notnullgames.ogg")
+  notnullgames = load_sound("/assets/notnullgames.ogg")
+  forgotten_spaceship = load_sound("/assets/forgotten_spaceship.ogg")
+  play_sound(forgotten_spaceship)
   
 proc update() {.null0.} =
   draw_text("Press A / B", 100, 110)
@@ -13,7 +16,7 @@ proc buttonDown(button: Button) {.null0.} =
   if button == BUTTON_A:
     play_sound(intro)
   if button == BUTTON_B:
-    play_sound(nnn)
+    play_sound(notnullgames)
 
 proc unload*() {.null0.} =
   echo "Ok, bye."
