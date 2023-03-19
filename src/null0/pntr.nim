0ece008eb077fbaf1d7f68f98592fef743caf1e0
David Konsumer <konsumer@jetboystudio.com>
2023-03-18 20:17:37 -0700

    update pntr

--- src/null0/pntr.nim
+++ src/null0/pntr.nim
@@ -1,10 +1,11 @@
-{.passC: "-Ivendor/ -DPNTR_SUPPORT_DEFAULT_FONT".}
+{.passC: "-Ivendor/".}
 
 {.emit: """
 #define PNTR_IMPLEMENTATION
 #define PNTR_PIXELFORMAT_ARGB
 #define PNTR_ENABLE_DEFAULT_FONT
 #define PNTR_ENABLE_TTF
+#define PNTR_ENABLE_FILTER_SMOOTH
 
 /*
 PNTR_LOAD_FILE macro should match, for easier physfs
@@ -89,20 +90,45 @@ const RAYWHITE* = pntr_color(r: 245, g: 245, b: 245, a: 255)
 proc new_image*(width: cint; height: cint): ptr pntr_image
 proc gen_image_color*(width: cint; height: cint; color: pntr_color): ptr pntr_image
 proc image_copy*(image: ptr pntr_image): ptr pntr_image
-proc image_from_image*(image: ptr pntr_image; x: cint; y: cint; width: cint; height: cint): ptr pntr_image
+proc image_from_image*(image: ptr pntr_image; x: cint; y: cint; width: cint;
+                           height: cint): ptr pntr_image
 proc unload_image*(image: ptr pntr_image)
 proc clear_background*(image: ptr pntr_image; color: pntr_color)
 proc draw_pixel*(dst: ptr pntr_image; x: cint; y: cint; color: pntr_color)
-proc draw_line*(dst: ptr pntr_image; startPosX: cint; startPosY: cint; endPosX: cint; endPosY: cint; color: pntr_color)
-proc draw_rectangle*(dst: ptr pntr_image; posX: cint; posY: cint; width: cint; height: cint; color: pntr_color)
-proc draw_rectangle_rec*(dst: ptr pntr_image; rect: pntr_rectangle; color: pntr_color)
-proc draw_circle*(dst: ptr pntr_image; centerX: cint; centerY: cint; radius: cint; color: pntr_color)
+proc draw_line*(dst: ptr pntr_image; startPosX: cint; startPosY: cint;
+                    endPosX: cint; endPosY: cint; color: pntr_color)
+proc draw_rectangle*(dst: ptr pntr_image; posX: cint; posY: cint; width: cint;
+                         height: cint; color: pntr_color)
+proc draw_rectangle_rec*(dst: ptr pntr_image; rect: pntr_rectangle;
+                             color: pntr_color)
+proc draw_circle*(dst: ptr pntr_image; centerX: cint; centerY: cint; radius: cint;
+                      color: pntr_color)
 proc draw_image*(dst: ptr pntr_image; src: ptr pntr_image; posX: cint; posY: cint)
-proc draw_image_rec*(dst: ptr pntr_image; src: ptr pntr_image; srcRect: pntr_rectangle; posX: cint; posY: cint)
-proc draw_text*(dst: ptr pntr_image; font: ptr pntr_font; text: cstring; posX: cint; posY: cint)
+proc draw_image_rec*(dst: ptr pntr_image; src: ptr pntr_image;
+                         srcRect: pntr_rectangle; posX: cint; posY: cint)
+proc draw_image_rotated*(dst: ptr pntr_image; src: ptr pntr_image; posX: cint;
+                             posY: cint; rotation: cfloat; offsetX: cfloat;
+                             offsetY: cfloat; filter: pntr_filter)
+proc draw_image_rec_rotated*(dst: ptr pntr_image; src: ptr pntr_image;
+                                 srcRect: pntr_rectangle; posX: cint; posY: cint;
+                                 rotation: cfloat; offsetX: cfloat; offsetY: cfloat;
+                                 filter: pntr_filter)
+proc draw_image_flipped*(dst: ptr pntr_image; src: ptr pntr_image; posX: cint;
+                             posY: cint; flipHorizontal: bool; flipVertical: bool)
+proc draw_image_rec_flipped*(dst: ptr pntr_image; src: ptr pntr_image;
+                                 srcRec: pntr_rectangle; posX: cint; posY: cint;
+                                 flipHorizontal: bool; flipVertical: bool)
+proc draw_image_rec_scaled*(dst: ptr pntr_image; src: ptr pntr_image;
+                                srcRect: pntr_rectangle; posX: cint; posY: cint;
+                                scaleX: cfloat; scaleY: cfloat; offsetX: cfloat;
+                                offsetY: cfloat; filter: pntr_filter)
+proc draw_image_scaled*(dst: ptr pntr_image; src: ptr pntr_image; posX: cint;
+                            posY: cint; scaleX: cfloat; scaleY: cfloat;
+                            offsetX: cfloat; offsetY: cfloat; filter: pntr_filter)
+proc draw_text*(dst: ptr pntr_image; font: ptr pntr_font; text: cstring; posX: cint;
+                    posY: cint)
 proc new_color*(r: uint8; g: uint8; b: uint8; a: uint8): pntr_color
 proc get_color*(hexValue: cuint): pntr_color
-proc color_get_rgba*(color: pntr_color; r: ptr uint8; g: ptr uint8; b: ptr uint8; a: ptr uint8)
 proc color_get_r*(color: pntr_color): uint8
 proc color_get_g*(color: pntr_color): uint8
 proc color_get_b*(color: pntr_color): uint8
@@ -114,39 +140,56 @@ proc color_set_a*(color: ptr pntr_color; a: uint8)
 proc image_get_color*(image: ptr pntr_image; x: cint; y: cint): pntr_color
 proc image_get_color_pointer*(image: ptr pntr_image; x: cint; y: cint): ptr pntr_color
 proc save_file*(fileName: cstring; data: pointer; bytesToWrite: cuint): bool
-proc image_to_pixelformat*(image: ptr pntr_image; dataSize: ptr cuint; pixelFormat: pntr_pixelformat): pointer
+proc image_to_pixelformat*(image: ptr pntr_image; dataSize: ptr cuint;
+                               pixelFormat: pntr_pixelformat): pointer
 proc save_image*(image: ptr pntr_image; fileName: cstring): bool
-proc save_image_to_memory*(image: ptr pntr_image; dataSize: ptr cuint): ptr uint8
-proc get_pixel_data_size*(width: cint; height: cint; pixelFormat: pntr_pixelformat): cint
+proc save_image_to_memory*(image: ptr pntr_image; dataSize: ptr cuint): pointer
+proc get_pixel_data_size*(width: cint; height: cint;
+                              pixelFormat: pntr_pixelformat): cint
 proc load_image*(fileName: cstring): ptr pntr_image
 proc load_image_from_memory*(fileData: pointer; dataSize: cuint): ptr pntr_image
-proc image_from_pixelformat*(data: pointer; width: cint; height: cint; pixelFormat: pntr_pixelformat): ptr pntr_image
+proc image_from_pixelformat*(data: pointer; width: cint; height: cint;
+                                 pixelFormat: pntr_pixelformat): ptr pntr_image
 proc get_error*(): cstring
 proc set_error*(error: cstring): pointer
-proc image_resize*(image: ptr pntr_image; newWidth: cint; newHeight: cint; filter: pntr_filter): ptr pntr_image
-proc image_color_replace*(image: ptr pntr_image; color: pntr_color; replace: pntr_color)
+proc image_resize*(image: ptr pntr_image; newWidth: cint; newHeight: cint;
+                       filter: pntr_filter): ptr pntr_image
+proc image_scale*(image: ptr pntr_image; scaleX: cfloat; scaleY: cfloat;
+                      filter: pntr_filter): ptr pntr_image
+proc image_color_replace*(image: ptr pntr_image; color: pntr_color;
+                              replace: pntr_color)
 proc color_tint*(color: pntr_color; tint: pntr_color): pntr_color
 proc image_color_tint*(image: ptr pntr_image; color: pntr_color)
 proc color_fade*(color: pntr_color; alpha: cfloat): pntr_color
 proc image_color_fade*(image: ptr pntr_image; alpha: cfloat)
 proc color_brightness*(color: pntr_color; factor: cfloat): pntr_color
 proc get_pixel_color*(srcPtr: pointer; srcPixelFormat: pntr_pixelformat): pntr_color
-proc set_pixel_color*(dstPtr: pointer; color: pntr_color; dstPixelFormat: pntr_pixelformat)
+proc set_pixel_color*(dstPtr: pointer; color: pntr_color;
+                          dstPixelFormat: pntr_pixelformat)
 proc load_default_font*(): ptr pntr_font
 proc unload_font*(font: ptr pntr_font)
+proc font_copy*(font: ptr pntr_font): ptr pntr_font
+proc font_scale*(font: ptr pntr_font; scaleX: cfloat; scaleY: cfloat;
+                     filter: pntr_filter): ptr pntr_font
 proc load_bmfont*(fileName: cstring; characters: cstring): ptr pntr_font
 proc load_bmfont_from_image*(image: ptr pntr_image; characters: cstring): ptr pntr_font
-proc load_bmfont_from_memory*(fileData: pointer; dataSize: cuint; characters: cstring): ptr pntr_font
+proc load_bmfont_from_memory*(fileData: pointer; dataSize: cuint;
+                                  characters: cstring): ptr pntr_font
 proc measure_text*(font: ptr pntr_font; text: cstring): cint
 proc measure_text_ex*(font: ptr pntr_font; text: cstring): pntr_vector
 proc gen_image_text*(font: ptr pntr_font; text: cstring): ptr pntr_image
-proc load_ttyfont*(fileName: cstring; glyphWidth: cint; glyphHeight: cint; characters: cstring): ptr pntr_font
-proc load_ttyfont_from_memory*(fileData: pointer; dataSize: cuint; glyphWidth: cint; glyphHeight: cint; characters: cstring): ptr pntr_font
-proc load_ttyfont_from_image*(image: ptr pntr_image; glyphWidth: cint; glyphHeight: cint; characters: cstring): ptr pntr_font
-proc load_file*(fileName: cstring; bytesRead: ptr cuint): ptr uint8
-proc unload_file*(fileData: ptr uint8)
+proc load_ttyfont*(fileName: cstring; glyphWidth: cint; glyphHeight: cint;
+                       characters: cstring): ptr pntr_font
+proc load_ttyfont_from_memory*(fileData: pointer; dataSize: cuint;
+                                   glyphWidth: cint; glyphHeight: cint;
+                                   characters: cstring): ptr pntr_font
+proc load_ttyfont_from_image*(image: ptr pntr_image; glyphWidth: cint;
+                                  glyphHeight: cint; characters: cstring): ptr pntr_font
+proc load_file*(fileName: cstring; bytesRead: ptr cuint): pointer
+proc unload_file*(fileData: pointer)
 proc load_ttffont*(fileName: cstring; fontSize: cint; fontColor: pntr_color): ptr pntr_font
-proc load_ttffont_from_memory*(fileData: pointer; dataSize: cuint; fontSize: cint; fontColor: pntr_color): ptr pntr_font
+proc load_ttffont_from_memory*(fileData: pointer; dataSize: cuint;
+                                   fontSize: cint; fontColor: pntr_color): ptr pntr_font
 proc color_invert*(color: pntr_color): pntr_color
 proc image_color_invert*(image: ptr pntr_image)
 proc color_alpha_blend*(dst: pntr_color; src: pntr_color): pntr_color
@@ -158,11 +201,19 @@ proc image_flip_vertical*(image: ptr pntr_image)
 proc image_flip_horizontal*(image: ptr pntr_image)
 proc color_contrast*(color: pntr_color; contrast: cfloat): pntr_color
 proc image_color_contrast*(image: ptr pntr_image; contrast: cfloat)
-proc image_alpha_mask*(image: ptr pntr_image; alphaMask: ptr pntr_image; posX: cint; posY: cint)
-proc image_resize_canvas*(image: ptr pntr_image; newWidth: cint; newHeight: cint; offsetX: cint; offsetY: cint; fill: pntr_color)
-proc image_rotate*(image: ptr pntr_image; rotation: cfloat): ptr pntr_image
-proc image_rotate_ex*(image: ptr pntr_image; rotation: cfloat; smooth: bool): ptr pntr_image
-proc gen_image_gradient_vertical*(width: cint; height: cint; top: pntr_color; bottom: pntr_color): ptr pntr_image
-proc gen_image_gradient_horizontal*(width: cint; height: cint; left: pntr_color; right: pntr_color): ptr pntr_image
-proc color_bilinear_interpolate*(color00: pntr_color; color01: pntr_color; color10: pntr_color; color11: pntr_color; coordinateX: cfloat; coordinateY: cfloat): pntr_color
+proc image_alpha_mask*(image: ptr pntr_image; alphaMask: ptr pntr_image;
+                           posX: cint; posY: cint)
+proc image_resize_canvas*(image: ptr pntr_image; newWidth: cint; newHeight: cint;
+                              offsetX: cint; offsetY: cint; fill: pntr_color)
+proc image_rotate*(image: ptr pntr_image; rotation: cfloat; filter: pntr_filter): ptr pntr_image
+proc gen_image_gradient_vertical*(width: cint; height: cint; top: pntr_color;
+                                      bottom: pntr_color): ptr pntr_image
+proc gen_image_gradient_horizontal*(width: cint; height: cint; left: pntr_color;
+                                        right: pntr_color): ptr pntr_image
+proc gen_image_gradient*(width: cint; height: cint; topLeft: pntr_color;
+                             topRight: pntr_color; bottomLeft: pntr_color;
+                             bottomRight: pntr_color): ptr pntr_image
+proc color_bilinear_interpolate*(color00: pntr_color; color01: pntr_color;
+                                     color10: pntr_color; color11: pntr_color;
+                                     coordinateX: cfloat; coordinateY: cfloat): pntr_color
 {.pop.}
